3c9f93d3744669811be23c97659ebddbe34827e1
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2020-09-27 13:46:45 +0300

    setupfiles,give an active style

---
+++ index.js
@@ -0,0 +1,40 @@
+function firstfun(){
+    let firstbtn1 = document.getElementById("firstbtn");
+    firstbtn1.classList.add("active");
+    let removeclass1 = document.getElementById("secondbtn");
+    let removeclass2 = document.getElementById("thirdbtn");
+    let removeclass3 = document.getElementById("fourthbtn");
+    removeclass1.classList.remove("active");
+    removeclass2.classList.remove("active");
+    removeclass3.classList.remove("active");
+}
+function secondfun(){
+    let firstbtn1 = document.getElementById("secondbtn");
+    firstbtn1.classList.add("active");
+    let removeclass1 = document.getElementById("firstbtn");
+    let removeclass2 = document.getElementById("thirdbtn");
+    let removeclass3 = document.getElementById("fourthbtn");
+    removeclass1.classList.remove("active");
+    removeclass2.classList.remove("active");
+    removeclass3.classList.remove("active");
+}
+function thirdfun(){
+    let firstbtn1 = document.getElementById("thirdbtn");
+    firstbtn1.classList.add("active");
+    let removeclass1 = document.getElementById("firstbtn");
+    let removeclass2 = document.getElementById("secondbtn");
+    let removeclass3 = document.getElementById("fourthbtn");
+    removeclass1.classList.remove("active");
+    removeclass2.classList.remove("active");
+    removeclass3.classList.remove("active");
+}
+function fourthfun(){
+    let firstbtn1 = document.getElementById("fourthbtn");
+    firstbtn1.classList.add("active");
+    let removeclass1 = document.getElementById("firstbtn");
+    let removeclass2 = document.getElementById("secondbtn");
+    let removeclass3 = document.getElementById("thirdbtn");
+    removeclass1.classList.remove("active");
+    removeclass2.classList.remove("active");
+    removeclass3.classList.remove("active");
+}
